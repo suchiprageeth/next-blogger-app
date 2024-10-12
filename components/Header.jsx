238c3763589ead52ac003b5ffd7751d96309884e
suchiprageeth <suchiruprageeth94@gmail.com>
2024-10-12 13:21:10 +0530

    Implement header with styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,42 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="py-5 px-5 md:px12 lg:px-28">
+      <div className="flex justify-between items-center">
+        <Image
+          className="w-[130px] sm:w-auto"
+          src={assets.logo}
+          width={180}
+          alt="Logo"
+        />
+        <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black">
+          Get Started
+          <Image src={assets.arrow} />
+        </button>
+      </div>
+      <div className="text-center my-8">
+        <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
+        <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+          doloribus ratione quas perferendis ipsum laborum nam facere doloremque
+        </p>
+        <form className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black">
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="pl-4 outline-none"
+          />
+          <button
+            type="submit"
+            className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white"
+          >
+            Subscribe
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
