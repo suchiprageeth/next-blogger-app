8bef571b5aac80e47d7fe3292d901da6552ebc70
suchiprageeth <suchiruprageeth94@gmail.com>
2024-10-12 14:49:06 +0530

    Implement blog item with styles

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+"use client";
+import BlogItem from "@/components/BlogItem";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <BlogItem />
     </>
   );
 }
